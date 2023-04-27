30e0809d7299aa2d4a352c8eb34102ccdfc98273
nikhitha <nikhitha.thimmannagaari@gmail.com>
2023-04-27 16:45:51 +0530

    weather application

---
+++ src/App.js
@@ -0,0 +1,105 @@
+/*import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+  //const url='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=51d8359a4ba489d5c3f3bd286aa819ab'
+  //const url=http:'https://api.openweathermap.org/data/2.5/weather?q=india&appid=51d8359a4ba489d5c3f3bd286aa819ab'
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top"></div>
+        <div className="bottom"></div>
+        <div className="location">
+          <p>India</p>
+        </div>
+        <div className="temp">
+          <h1>60 F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+        <div className="feels">
+          <p>60 F</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          <p>12 MPH</p>
+        </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+  );
+}
+
+export default App;
+*/
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
